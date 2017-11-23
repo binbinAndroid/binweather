ea6ee94acfd712dc58eac001e4722a617bbf40b4
binbinAndroid <964393797@qq.com>
2017-11-23 14:42:28 +0800

    完成遍历省市县三级列表的功能

--- app/src/main/java/com/binweather/android/db/County.java
+++ app/src/main/java/com/binweather/android/db/County.java
@@ -6,9 +6,9 @@ import org.litepal.crud.DataSupport;
  * Created by 彬少 on 2017/11/23.
  */
 
-public class Country extends DataSupport {
+public class County extends DataSupport {
     private int id;
-    private String countryName;
+    private String countyName;
     private String weatherId;
     private int cityId;
     public int getId(){
@@ -17,11 +17,11 @@ public class Country extends DataSupport {
     public void setId(int id){
         this.id=id;
     }
-    public String getCountryName(){
-        return countryName;
+    public String getCountyName(){
+        return countyName;
     }
-    public void setCountryName(String countryName){
-        this.countryName=countryName;
+    public void setCountyName(String countyName){
+        this.countyName=countyName;
     }
     public String getWeatherId(){
         return weatherId;
